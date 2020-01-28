0eebf4e368d28b6ebe88b4151b56734cf9fe5052
Ubuntu <ubuntu@ip-172-31-30-252.us-east-2.compute.internal>
2020-01-28 11:30:12 +0000

    second commit

--- index.php
+++ index.php
@@ -26,7 +26,7 @@ session_start();
                <div class="container">
                    <center>
                    <div id="bannerContent">
-                       <h1>We sell Anand .</h1>
+                       <h1> Anand testing webesite .</h1>
                        <p>Flat 40% OFF on all premium brands.</p>
                        <a href="products.php" class="btn btn-danger">Shop Now</a>
                    </div>
