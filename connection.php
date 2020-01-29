5929574b075629b18b8acfa3acf7e4aba92d8613
Ubuntu <ubuntu@ip-172-31-30-252.us-east-2.compute.internal>
2020-01-29 16:39:52 +0000

    connection updated to admin1 user

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$con=mysqli_connect("localhost","admin","admin","shop") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","admin1","admin1","shop") or die(mysqli_error($con));
 //$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 ?>
